c927dee37a0325de3e74dd53bd354bf1b20c2fd6
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2024-06-26 23:46:51 +0300

    test: Added Additional Handler to Test Order

---
+++ tests/webfiori/tests/error/SampleHandler3.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace webfiori\tests\error;
+
+use webfiori\error\AbstractHandler;
+
+class SampleHandler3 extends AbstractHandler {
+    public function __construct() {
+        parent::__construct();
+        $this->setPriority(5);
+        $this->setName('H3');
+    }
+    public function handle() {
+        define('SampleHandler3', 'Yes');
+    }
+
+    public function isActive(): bool {
+        return true;
+    }
+
+    public function isShutdownHandler(): bool {
+        return true;
+    }
+}
